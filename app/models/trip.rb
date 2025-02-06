4fdfeedf2c85d00402f5c1c1a4be85c8ddab39a5
Omar Haizoun <omarhaizoun@Omars-MacBook-Air.local>
2025-02-06 19:09:04 +0100

    fINALIZE backend models with: validations, associations and cleanup callback to destroy reviews from mongo if trip deleted

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,18 @@
 class Trip < ApplicationRecord
   belongs_to :driver, class_name: "User"
-
   belongs_to :vehicle
-
   has_many :passenger_bookings, dependent: :destroy
 
   def reviews
     Review.where(trip_id: id)
+  end
+
+  # Before destroying trip, delete its reviews from MongoDB.
+  before_destroy :cleanup_reviews
+
+  private
+
+  def cleanup_reviews
+    Review.where(trip_id: id).delete_all
   end
 end
