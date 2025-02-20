828461e8cafd2e5d5198c127e6886d7a13c7b394
Omar Haizoun <omarhaizoun@Omars-MacBook-Air.local>
2025-02-20 18:30:09 +0100

    Improved password security without using gem for time issues, and created tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_many :passenger_bookings, foreign_key: "passenger_id", dependent: :destroy
   has_many :vehicles, dependent: :destroy
 
-  # Allow drivers (or users choosing "both") to manage their vehicles via nested attributes
   accepts_nested_attributes_for :vehicles, allow_destroy: true, reject_if: :all_blank
 
   # Validations
@@ -18,8 +17,23 @@ class User < ApplicationRecord
   validates :role, inclusion: { in: %w[driver passenger both employee admin],
     message: "doit être 'driver', 'passenger', 'both', 'employee' ou 'admin'" }
 
-  # Helper method to retrieve trips booked as a passenger
+  validate :password_complexity, if: :password_required?
+
   def booked_trips
     Trip.where(id: passenger_bookings.pluck(:trip_id))
+  end
+
+  private
+
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+
+  def password_complexity
+    return if password.blank?
+    unless password.match(/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}/)
+      errors.add :password, "must include at least one uppercase letter, one lowercase letter, one digit, and one special character, and be at least 8 characters long"
+    end
   end
 end
